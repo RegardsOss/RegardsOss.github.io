ce5ede0337438123e76b2a4ac0383e09c6fb9d8b
Sébastien Binda <sebastien.binda@csgroup.eu>
2024-09-03 09:55:51 +0200

    fix(ci): fix ci build

--- jenkins/nginx/buildTagAndPush.sh
+++ jenkins/nginx/buildTagAndPush.sh
@@ -3,7 +3,7 @@
 # If we are on branch master or a branch starting with release, tag the image with the version from package.json
 if [[ "${BRANCH_NAME}" == release* || "${BRANCH_NAME}" == "master"  || "${BRANCH_NAME}" == "develop" ]]
 then
-  TAG=$(./getPackageVersion.sh ../../webapp)
+  TAG=$(./getPackageVersion.sh ../../)
 # Else use the branch name
 else
   TAG=$(echo ${BRANCH_NAME} | tr -cd '[[:alnum:]]._-')
